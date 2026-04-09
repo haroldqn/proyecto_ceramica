2a066d4aa53330ce916f658326738bf908f730ae
Zogurf <jairvillaltav08@gmail.com>
2026-04-08 22:47:58 -0500

    Creacion de tablas necesarias para el login

---
+++ backend/src/main/java/com/example/backend/models/User.java
@@ -0,0 +1,27 @@
+package com.example.backend.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter
+@AllArgsConstructor @NoArgsConstructor
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_user")
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+    @Column(nullable = false)
+    private String password;
+
+    @ManyToOne
+    @JoinColumn(name = "id_role")
+    private Role role ;
+}
